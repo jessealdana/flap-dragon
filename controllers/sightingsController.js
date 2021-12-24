f859c24433af9c3835e8a34653b2b099fda5b0e2
Jesse Aldana <jesse.aldana@gmail.com>
2021-12-23 20:02:33 -0800

    fixed paths

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-// Defining methods for the postsController
+// Defining methods for the sightingsController
 module.exports = {
   findAll: function(req, res) {
     db.Sighting.find(req.query)
--- routes/api/sightings.js
+++ routes/api/sightings.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const sightingsController = require("../../controllers/sightingsController");
 
-// Matches with "/api/posts"
+// Matches with "/api/sightings"
 router
   .route("/")
   .get(sightingsController.findAll)
   .post(sightingsController.create);
 
-// Matches with "/api/posts/:id"
+// Matches with "/api/sightings/:id"
 router
   .route("/:id")
   .get(sightingsController.findById)
